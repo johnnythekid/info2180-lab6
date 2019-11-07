1d39af2867975c6b8d67148c709fea44ed547a36
Jon-Ross Hamilton <jonhamilton87@outlook.com>
2019-11-06 21:16:01 -0500

    Changed

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-var xhr = new XMLHttpRequest();
-var site="http://localhost:8080/superheroes.php";
-xhr.open("GET",site,true);
+var xhr=new XMLHttpRequest();
+var url = "superheores.php"; 
+xhr.onreadystatechange = doSomething; 
+xhr.open('GET', url); 
 xhr.send();
-console.log(xhr.state);
+function doSomething() { 
+    if (xhr.readyState === XMLHttpRequest.DONE) { 
+        if (xhr.status === 200) { 
+            var response = xhr.responseText; 
+             alert(response); 
+        }  else{
+            alert('There was a problem with the request.'); 
+            console.log(xhr.status);
 
+        }
+    }
+}
